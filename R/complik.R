94b53ece94ed0d3000756582bd557e33fb81e5ed
Klaus K. Holst <klaus@holst.it>
2020-03-05 11:30:02 +0000

    version 1.6.7

--- R/complik.R
+++ R/complik.R
@@ -13,9 +13,11 @@
 ##' argument which overrides \code{k} and \code{type} but gives complete
 ##' flexibility in the specification of the composite likelihood
 ##' @param messages Control amount of messages printed
-##' @param \dots Additional arguments parsed on to lower-level functions
 ##' @param estimator Model (pseudo-likelihood) to use for the pairs/groups
-##' @return An object of class \code{clprobit} inheriting methods from \code{lvm}
+##' @param quick If TRUE the parameter estimates are calculated but all additional
+##' information such as standard errors are skipped
+##' @param \dots Additional arguments parsed on to lower-level functions
+##' @return An object of class \code{estimate.complik} inheriting methods from \code{lvm}
 ##' @author Klaus K. Holst
 ##' @seealso estimate
 ##' @keywords models regression
@@ -25,8 +27,9 @@
 ##' ordinal(m,K=2) <- ~y1+y2+y3
 ##' d <- sim(m,50,seed=1)
 ##' e1 <- complik(m,d,control=list(trace=1),type="all")
-complik <- function(x,data,k=2,type=c("nearest","all"),pairlist,messages=0,estimator="normal",
-             ...) {
+complik <- function(x, data, k=2, type=c("all","nearest"), pairlist,
+             messages=0, estimator="normal", quick=FALSE, ...) {
+    
     y <- setdiff(endogenous(x),latent(x))
     binsurv <- rep(FALSE,length(y))
     for (i in 1:length(y)) {
@@ -58,10 +61,10 @@ complik <- function(x,data,k=2,type=c("nearest","all"),pairlist,messages=0,estim
         for (i in seq(ncol(mypar0)))
             mypar <- c(mypar, list(mypar0[,i]))
     }
-
     nblocks <- length(mypar)
+    
     mydata0 <- data[,,drop=FALSE]
-    mydata <-  as.data.frame(matrix(NA, nblocks*nrow(data), ncol=ncol(data)))
+    mydata <-  as.data.frame(matrix(NA, nblocks*nrow(data), ncol=ncol(data)))    
     names(mydata) <- names(mydata0)
     for (i in 1:ncol(mydata)) {
         if (is.factor(data[,i])) {
@@ -70,7 +73,7 @@ complik <- function(x,data,k=2,type=c("nearest","all"),pairlist,messages=0,estim
         if (survival::is.Surv(data[,i])) {
             S <- data[,i]
             for (j in 2:nblocks) S <- rbind(S,data[,i])
-            S[,1] <- NA
+            S[] <- NA
             mydata[,i] <- S
         }
     }
@@ -78,7 +81,8 @@ complik <- function(x,data,k=2,type=c("nearest","all"),pairlist,messages=0,estim
         data0 <- data;
         for (i in binsurvpos[-mypar[[ii]]]) {
             if (survival::is.Surv(data[,i])) {
-                S <- data0[,i]; S[,1] <- NA
+                S <- data0[,i]
+                S[] <- NA
                 data0[,i] <- S
             } else {
                 data0[,i] <- NA
@@ -88,43 +92,64 @@ complik <- function(x,data,k=2,type=c("nearest","all"),pairlist,messages=0,estim
         mydata[(1:nrow(data))+(ii-1)*nrow(data),] <- data0
     }
     suppressWarnings(e0 <- estimate(x,data=mydata,estimator=estimator,missing=TRUE,messages=messages,
+                                    hessian=!quick,
                                     ...))
-
-    S <- score(e0,indiv=TRUE)
-    nd <- nrow(data)
-    block1 <- which((1:nd)%in%(rownames(S)))
-    blocks <- sapply(1:nblocks, function(x) 1:length(block1)+length(block1)*(x-1))
-    if (nblocks==1) {
-        Siid <- S
-    } else {
-        Siid <- matrix(0,nrow=length(block1),ncol=ncol(S))
-        for (j in 1:ncol(blocks)) {
-            Siid <- Siid+S[blocks[,j],]
+    e0$n <- nrow(data)
+    e0$eigenvalues <- rep(1,nrow(e0$coef))
+    e0$coef[,2:4] <- NA
+    if (!quick) {
+        S <- score(e0,indiv=TRUE)
+        nd <- nrow(data)
+        block1 <- which((1:nd)%in%(rownames(S)))
+        blocks <- sapply(1:nblocks, function(x) 1:length(block1)+length(block1)*(x-1))
+        if (nblocks==1) {
+            Siid <- S
+        } else {
+            Siid <- matrix(0,nrow=length(block1),ncol=ncol(S))
+            for (j in 1:ncol(blocks)) {
+                Siid <- Siid+S[blocks[,j],]
+            }
         }
+        ##B <- solve(information(e0, type="hessian"))
+        D <- numDeriv::jacobian(function(p) score(e0, p=p), coef(e0), method=lava.options()$Dmethod)
+        B <- -Inverse(D)
+        A <- crossprod(Siid)
+        e0$bread <- B
+        e0$meat <- A
+        e0$iidscore <- Siid
+        e0$blocks <- blocks
+        BA <- B%*%A
+        eig <- eigen(BA)$values
+        e0$eigenvalues <- eig
+        e0$vcov <- BA%*%B
+        cc <- e0$coef;
+        cc[,2] <- sqrt(diag(e0$vcov))
+        cc[,3] <- cc[,1]/cc[,2]; cc[,4] <- 2*(1-pnorm(abs(cc[,3])))
+        e0$coef <- cc
     }
-    iI <- solve(information(e0,type="hessian"))
-    J <- t(Siid)%*%(Siid)
-    e0$iidscore <- Siid
-    e0$blocks <- blocks
-    e0$vcov <- iI%*%J%*%iI ## thetahat-theta0 :=(asymp) I^-1*S => var(thetahat) = iI*var(S)*iI
-    cc <- e0$coef; cc[,2] <- sqrt(diag(e0$vcov))
-    cc[,3] <- cc[,1]/cc[,2]; cc[,4] <- 2*(1-pnorm(abs(cc[,3])))
-    e0$coef <- cc
-    e0$bread <- iI
     class(e0) <- c("estimate.complik",class(e0))
     return(e0)
 }
 
 
+##' @export
 score.estimate.complik <- function(x,indiv=FALSE,...) {
     if (!indiv)
         return(colSums(x$iidscore))
     x$iidscore
 }
 
+##' @export
 iid.estimate.complik <- function(x,...) {
     iid.default(x,bread=x$bread,...)
 }
 
-
+##' @export
+logLik.estimate.complik <- function(object,...) {    
+    with(object, structure(opt$objective,
+              nall=n,
+              nobs=n,
+              df=sum(eigenvalues),
+              class="logLik"))
+}
 
